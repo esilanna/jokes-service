88ce0ca757ab4ed05163036b1353af1b8cd04153
Annalise Grekalski <grekalski842@gmail.com>
2023-02-02 15:23:40 -0500

    added filter for get method

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
 const express = require('express');
 const app = express();
 const { Joke } = require('./db');
+const { Op } = require('sequelize');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+// app.get('/jokes', async (req, res, next) => {
+//   try {
+//     // TODO - filter the jokes by tags and content
+//     const jokes = await Joke.findAll();
+//     res.send(jokes);
+//   } catch (error) {
+//     console.error(error);
+//     next(error)
+//   }
+// });
+// use where, op.like array
 app.get('/jokes', async (req, res, next) => {
   try {
+    if(Object.keys(req.query).length===0){
+            const jokes = await Joke.findAll({});
+            res.send(jokes);
+          }
+
+    else {
+    //const where = {};
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const jokes = await Joke.findAll(
+      {where: {
+        tags: {[Op.like]: `%${req.query.tags}%`}
+      }}
+      // {where: {
+      //   tags: {[Op.like]: `%${req.query.tags}%`}
+      // }}
+
+    );
     res.send(jokes);
+  }
+    // app.get('/jokes', async (req, res, next) => {
+    //   try {
+    //     if(Object.keys(req.query).length===0){
+    //       const jokes = await Joke.findAll({});
+    //       res.send(jokes);
+    //     }
+    //     else {
+    //       const where = {};
+    //       const jokes = await Joke.findAll(
+    //         {where:{tags:'dad'}
+    
+    //         }
+    //       )
+          
+    //     }
+
   } catch (error) {
     console.error(error);
     next(error)
